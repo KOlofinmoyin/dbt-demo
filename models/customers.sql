07544364fce68717ebd028b0b015a234dd61ae93
Pete Olof <peter.olofinmoyin@gmail.com>
2024-06-26 11:41:10 +0000

    separated cleanup & transformation logic.

--- models/customers.sql
+++ models/customers.sql
@@ -4,28 +4,11 @@
   )
 }}
 
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from `dbt-tutorial`.jaffle_shop.customers
+with customers as (select * from {{ref('stg_customers')}}
 
 ),
 
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from `dbt-tutorial`.jaffle_shop.orders
-
-),
+orders as (select * from {{ref('stg_orders')}}),
 
 customer_orders as (
 
